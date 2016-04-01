03e73b674ea9a4102bd5b92417b66077eb5c3567
lat9 <lat9@vinosdefrutastropicales.com>
2016-04-01 15:16:37 -0400

    #9: Add Misc. Cost order-total
    Include a Misc. Cost order-total as a complement to the
    one_time_discount.

---
+++ 4_optional_items/2_misc_cost/1_new_files/includes/languages/english/modules/order_total/ot_misc_cost.php
@@ -0,0 +1,6 @@
+<?php
+// -----
+// Gather any Miscellaneous Cost for an order, an order-total created by lat9 (http://vinosdefrutastropicales.com)
+//
+define('MODULE_ORDER_TOTAL_MISC_COST_TITLE', 'Misc. Cost');
+define('MODULE_ORDER_TOTAL_MISC_COST_DESCRIPTION', 'A miscellaneous cost associated with the order.');
