af2a913e6a54c2971e7519c34edcd825717c6fc7
lat9 <lat9@vinosdefrutastropicales.com>
2017-02-17 07:38:35 -0500

    #43: Installation restructuring
    Re-layout

--- YOUR_ADMIN/includes/auto_loaders/config.eo.php
+++ YOUR_ADMIN/includes/auto_loaders/config.eo.php
@@ -1,8 +1,13 @@
 <?php
-if (!defined('IS_ADMIN_FLAG')) { die('Illegal Access'); }
+// -----
+// Part of the Edit Orders plugin for Zen Cart, provided by lat9 and others.
+// 
+if (!defined ('IS_ADMIN_FLAG')) { 
+    die ('Illegal Access'); 
+}
 
-$autoLoadConfig[0][] = array(
-	'autoType'=>'class',
-	'loadFile'=>'mock_cart.php',
-	'classPath'=> DIR_FS_ADMIN . DIR_WS_CLASSES
+$autoLoadConfig[0][] = array (
+    'autoType' => 'class',
+    'loadFile' => 'mock_cart.php',
+    'classPath' => DIR_FS_ADMIN . DIR_WS_CLASSES
 );
