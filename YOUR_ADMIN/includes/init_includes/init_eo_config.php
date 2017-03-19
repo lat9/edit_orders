aa8a0638407540a37bc72d1159f1ea950ef5a328
lat9 <lat9@vinosdefrutastropicales.com>
2017-03-19 10:29:53 -0400

    #47: Update readme/config version to 4.2.1
    Per the title.

--- YOUR_ADMIN/includes/init_includes/init_eo_config.php
+++ YOUR_ADMIN/includes/init_includes/init_eo_config.php
@@ -7,7 +7,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define ('EO_CURRENT_VERSION', '4.2.1-beta1');
+define ('EO_CURRENT_VERSION', '4.2.1');
 
 $configurationGroupTitle = 'Edit Orders';
 $configuration = $db->Execute (
