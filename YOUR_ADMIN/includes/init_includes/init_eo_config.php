f623a423ceaee28938e06d171624e3a85b5eff98
lat9 <lat9@vinosdefrutastropicales.com>
2017-03-20 11:52:01 -0400

    #48: Correct PHP parse issue
    ... introduced in v4.2.1.

--- YOUR_ADMIN/includes/init_includes/init_eo_config.php
+++ YOUR_ADMIN/includes/init_includes/init_eo_config.php
@@ -7,7 +7,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define ('EO_CURRENT_VERSION', '4.2.1');
+define ('EO_CURRENT_VERSION', '4.2.2');
 
 $configurationGroupTitle = 'Edit Orders';
 $configuration = $db->Execute (
