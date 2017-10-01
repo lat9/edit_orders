5569f6ec6ac35c0928579c5de3520f1b3cfce6bd
lat9 <lat9@vinosdefrutastropicales.com>
2017-10-01 10:21:03 -0400

    Update versions to 4.3.0 for release
    Update the readme and the plugin's auto-installer.

--- YOUR_ADMIN/includes/init_includes/init_eo_config.php
+++ YOUR_ADMIN/includes/init_includes/init_eo_config.php
@@ -7,7 +7,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define ('EO_CURRENT_VERSION', '4.3.0-beta2');
+define ('EO_CURRENT_VERSION', '4.3.0');
 
 // -----
 // Only update configuration when an admin is logged in.
