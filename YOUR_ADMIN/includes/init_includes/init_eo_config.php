95d63cf269b1cecc4fdfd5ee3685d853d6f00bdd
lat9 <lat9@vinosdefrutastropicales.com>
2018-03-09 14:29:58 -0500

    Update versions for 4.3.3 release
    ... in the readme and the initialization script.

--- YOUR_ADMIN/includes/init_includes/init_eo_config.php
+++ YOUR_ADMIN/includes/init_includes/init_eo_config.php
@@ -7,7 +7,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('EO_CURRENT_VERSION', '4.3.3-beta1');
+define('EO_CURRENT_VERSION', '4.3.3');
 
 // -----
 // Only update configuration when an admin is logged in.
