8ca93ab200f8bb6183cc293e3025673b3c4a13af
lat9 <lat9@vinosdefrutastropicales.com>
2018-04-08 09:14:29 -0400

    Update readme/config version for 4.3.4 release
    Updating versions and release dates.

--- YOUR_ADMIN/includes/init_includes/init_eo_config.php
+++ YOUR_ADMIN/includes/init_includes/init_eo_config.php
@@ -7,15 +7,14 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('EO_CURRENT_VERSION', '4.3.4-beta3');
+define('EO_CURRENT_VERSION', '4.3.4');
 
 // -----
 // Only update configuration when an admin is logged in.
 //
 if (isset($_SESSION['admin_id'])) {
-
     $configurationGroupTitle = 'Edit Orders';
-    $configuration = $db->Execute (
+    $configuration = $db->Execute(
         "SELECT configuration_group_id 
            FROM " . TABLE_CONFIGURATION_GROUP . " 
           WHERE configuration_group_title = '$configurationGroupTitle' 
@@ -29,7 +28,7 @@ if (isset($_SESSION['admin_id'])) {
                 ('$configurationGroupTitle', '$configurationGroupTitle', '1', '1')"
         );
         $cgi = $db->Insert_ID(); 
-        $db->Execute ("UPDATE " . TABLE_CONFIGURATION_GROUP . " SET sort_order = $cgi WHERE configuration_group_id = $cgi;");
+        $db->Execute("UPDATE " . TABLE_CONFIGURATION_GROUP . " SET sort_order = $cgi WHERE configuration_group_id = $cgi;");
     } else {
         $cgi = $configuration->fields['configuration_group_id'];
     }
@@ -37,43 +36,43 @@ if (isset($_SESSION['admin_id'])) {
     // ----
     // If not already set, record the configuration's current version in the database.
     //
-    if (!defined ('EO_VERSION')) {
-        $db->Execute (
+    if (!defined('EO_VERSION')) {
+        $db->Execute(
             "INSERT INTO " . TABLE_CONFIGURATION . " 
                 (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, set_function) 
              VALUES 
                 ('Edit Orders Version', 'EO_VERSION', '" . EO_CURRENT_VERSION . "', 'The currently-installed version of the plugin.', $cgi, 1, now(), 'trim(')"
         );
 
-        $db->Execute (
+        $db->Execute(
             "INSERT INTO " . TABLE_CONFIGURATION . " 
                 ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
              VALUES 
                 ( 'Reset Totals on Update &mdash; Default', 'EO_TOTAL_RESET_DEFAULT', 'off', 'Choose the default value for the <em>Reset totals prior to update</em> checkbox.  If your store uses order-total modules that perform tax-related recalculations (like &quot;Group Pricing&quot;), set this value to <b>on</b>.', $cgi, 5, now(), NULL, 'zen_cfg_select_option(array(\'on\', \'off\'),')"
         );
         
-        $db->Execute (
+        $db->Execute(
             "INSERT INTO " . TABLE_CONFIGURATION . " 
                 ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
              VALUES 
                 ( 'Use a mock shopping cart?', 'EO_MOCK_SHOPPING_CART', 'true', 'When enabled, a mock shopping cart is created which reads product information from the current order. Many of the 3rd party \"Discount\" Order Total modules were not designed to be run from the Zen Cart administrative interface and require this option to be enabled.<br /><br />The mock shopping cart only provides the get_products and in_cart methods.<br /><br />If installed order total or shipping modules require additional methods from the shopping cart, the mock cart should be disabled.', $cgi, 10, now(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),')"
         );
       
-        $db->Execute (
+        $db->Execute(
             "INSERT INTO " . TABLE_CONFIGURATION . " 
                 ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
              VALUES 
                 ( 'Strip tags from the shipping module name?', 'EO_SHIPPING_DROPDOWN_STRIP_TAGS', 'true', 'When enabled, HTML and PHP tags present in the title of a shipping module are removed from the text displayed in the shipping dropdown menu.<br /><br />If partial or broken tags are present in the title it may result in the removal of more text than expected. If this happens, you will need to update the affected shipping module(s) or disable this option.', $cgi, 11, now(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),')"
         );
       
-        $db->Execute (
+        $db->Execute(
             "INSERT INTO " . TABLE_CONFIGURATION . " 
                 ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
              VALUES 
                 ( 'Debug Action Level', 'EO_DEBUG_ACTION_LEVEL', '0', 'When enabled when actions are performed by Edit Orders additional debugging information will be stored in a log file.<br /><br />Enabling debugging will result in a large number of created log files and may adversely affect server performance. Only enable this if absolutely necessary!', $cgi, 12, now(), NULL, 'eo_debug_action_level_list(')"
         );
       
-        $db->Execute (
+        $db->Execute(
             "INSERT INTO " . TABLE_CONFIGURATION . " 
                 ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
              VALUES 
@@ -83,9 +82,9 @@ if (isset($_SESSION['admin_id'])) {
         // -----
         // Register the admin pages for the plugin.
         //
-        $next_sort = $db->Execute ("SELECT MAX(sort_order) as max_sort FROM " . TABLE_ADMIN_PAGES . " WHERE menu_key='customers'", false, false, 0, true);
-        zen_register_admin_page ('editOrders', 'BOX_CONFIGURATION_EDIT_ORDERS', 'FILENAME_EDIT_ORDERS', '', 'customers', 'N', $next_sort->fields['max_sort'] + 1);
-        zen_register_admin_page ('configEditOrders', 'BOX_CONFIGURATION_EDIT_ORDERS', 'FILENAME_CONFIGURATION', "gID=$cgi", 'configuration', 'Y', $cgi);
+        $next_sort = $db->Execute("SELECT MAX(sort_order) as max_sort FROM " . TABLE_ADMIN_PAGES . " WHERE menu_key='customers'", false, false, 0, true);
+        zen_register_admin_page('editOrders', 'BOX_CONFIGURATION_EDIT_ORDERS', 'FILENAME_EDIT_ORDERS', '', 'customers', 'N', $next_sort->fields['max_sort'] + 1);
+        zen_register_admin_page('configEditOrders', 'BOX_CONFIGURATION_EDIT_ORDERS', 'FILENAME_CONFIGURATION', "gID=$cgi", 'configuration', 'Y', $cgi);
         
         define('EO_INIT_FILE_MISSING', '1');  //-Set so that the notifier checks will run upon initial installation.
         define('EO_VERSION', '0.0.0');
@@ -97,7 +96,7 @@ if (isset($_SESSION['admin_id'])) {
     //
     } else {
         if (EO_VERSION <= '4.1.1') {
-            $db->Execute (
+            $db->Execute(
                 "DELETE FROM " . TABLE_CONFIGURATION . "
                   WHERE configuration_key = 'EO_SHIPPING_TAX'
                   LIMIT 1"
@@ -105,14 +104,14 @@ if (isset($_SESSION['admin_id'])) {
         }
         
         if (version_compare(EO_VERSION, '4.3.0', '<')) {
-            $db->Execute (
+            $db->Execute(
                 "INSERT IGNORE INTO " . TABLE_CONFIGURATION . " 
                     ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
                  VALUES 
                     ( 'Reset Totals on Update &mdash; Default', 'EO_TOTAL_RESET_DEFAULT', 'off', 'Choose the default value for the <em>Reset totals prior to update</em> checkbox.  If your store uses order-total modules that perform tax-related recalculations (like &quot;Group Pricing&quot;), set this value to <b>on</b>.', $cgi, 5, now(), NULL, 'zen_cfg_select_option(array(\'on\', \'off\'),')"
             );
           
-            $db->Execute (
+            $db->Execute(
                 "INSERT IGNORE INTO " . TABLE_CONFIGURATION . " 
                     ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
                  VALUES 
@@ -125,13 +124,13 @@ if (isset($_SESSION['admin_id'])) {
         }
         
         if (version_compare(EO_VERSION, '4.3.4', '<')) {
-            $db->Execute (
+            $db->Execute(
                 "INSERT IGNORE INTO " . TABLE_CONFIGURATION . " 
                     ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
                  VALUES 
                     ( 'Product Price Calculation &mdash; Method', 'EO_PRODUCT_PRICE_CALC_METHOD', 'Auto', 'Choose the <em>method</em> that &quot;EO&quot; uses to calculate product prices when an order is updated, one of:<ol><li><b>Auto</b>: Each product-price is re-calculated.  If your products have attributes, this enables changes to a product\'s attributes to automatically update the associated product-price.</li><li><b>Manual</b>: Each product-price is based on the <b><i>admin-entered price</i></b> for the product.</li><li><b>Choose</b>: The product-price calculation method varies on an order-by-order basis, via the &quot;tick&quot; of a checkbox.  The default method used (<em>Auto</em> vs. <em>Manual</em> is defined by the <em>Product Price Calculation &mdash; Default</em> setting.</li></ol>', $cgi, 20, now(), NULL, 'zen_cfg_select_option(array(\'Auto\', \'Manual\', \'Choose\'),')"
             );
-            $db->Execute (
+            $db->Execute(
                 "INSERT IGNORE INTO " . TABLE_CONFIGURATION . " 
                     ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
                  VALUES 
