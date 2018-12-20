2c06c71e4fc4bb57c114b7102037a14915053f8c
lat9 <lat9@vinosdefrutastropicales.com>
2018-12-20 07:40:44 -0500

    #89, #90: Update EO version to 4.3.6-beta2

--- YOUR_ADMIN/includes/init_includes/init_eo_config.php
+++ YOUR_ADMIN/includes/init_includes/init_eo_config.php
@@ -7,7 +7,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('EO_CURRENT_VERSION', '4.3.6-beta1');
+define('EO_CURRENT_VERSION', '4.3.6-beta2');
 
 // -----
 // Only update configuration when an admin is logged in.
