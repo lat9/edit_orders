ba8368fa9d3d2e05875cf2eac7b62ef5a30b1a0b
lat9 <lat9@vinosdefrutastropicales.com>
2018-12-22 14:09:16 -0500

    #93: Add tax class to ot_misc_cost
    ... corrects "funky" tax calculations.

--- includes/modules/order_total/ot_misc_cost.php
+++ includes/modules/order_total/ot_misc_cost.php
@@ -49,56 +49,61 @@ class ot_misc_cost
     {
         $this->code = 'ot_misc_cost';
         $this->title = MODULE_ORDER_TOTAL_MISC_COST_TITLE;
+        if (IS_ADMIN_FLAG === true) {
+            if (defined('MODULE_ORDER_TOTAL_MISC_COST_STATUS') && !defined('MODULE_ORDER_TOTAL_MISC_COST_TAX_CLASS')) {
+                $current_page = $GLOBALS['current_page'];
+                if ($current_page == FILENAME_MODULES . '.php') {
+                    $this->title .= ' <span class="alert">' . MODULE_ORDER_TOTAL_MISC_COST_KEYS_MISSING . '</span>';
+                } else {
+                    $GLOBALS['messageStack']->add_session(MODULE_ORDER_TOTAL_MISC_COST_MISSING_TAXCLASS, 'warning');
+                }
+            }
+        }
         $this->description = MODULE_ORDER_TOTAL_MISC_COST_DESCRIPTION;
-        $this->sort_order = (int)MODULE_ORDER_TOTAL_MISC_COST_SORT_ORDER;
+        $this->sort_order = (defined('MODULE_ORDER_TOTAL_MISC_COST_SORT_ORDER')) ? ((int)MODULE_ORDER_TOTAL_MISC_COST_SORT_ORDER) : '';
+        $this->tax_class_id = (defined('MODULE_ORDER_TOTAL_MISC_COST_TAX_CLASS')) ? ((int)MODULE_ORDER_TOTAL_MISC_COST_TAX_CLASS) : 0;
+        
         $this->output = array();
     }
 
     public function process() 
     {
         global $db, $currencies, $order;
-
-        if (IS_ADMIN_FLAG === true && isset($_GET['oID'])) {
-            $oID = (int)$_GET['oID'];
-            $query = $db->Execute(
-                "SELECT `title`, `text`, `value` 
-                   FROM " . TABLE_ORDERS_TOTAL . "
-                  WHERE orders_id = $oID
-                    AND class = 'ot_misc_cost'
-                  LIMIT 1"
-            );
-            if (!$query->EOF) {
-                // Do we allow changing the "title"?
-                if (MODULE_ORDER_TOTAL_MISC_COST_CHANGE_TITLE === 'true') {
-                    $this->title = rtrim($query->fields['title'], ':');
+        if (IS_ADMIN_FLAG === true) {
+            for ($i = 0, $n = count($order->totals); $i < $n; $i++) {
+                if ($order->totals[$i]['class'] == 'ot_misc_cost') {
+                    $value = (float)$order->totals[$i]['value'];
+                    if ($value != 0) {
+                        if (MODULE_ORDER_TOTAL_MISC_COST_CHANGE_TITLE == 'false') {
+                            $order->totals[$i]['title'] = $this->title;
+                        } else {
+                            $order->totals[$i]['title'] = rtrim($order->totals[$i]['title'], ' :');
+                        }
+                        
+                        $order->totals[$i]['value'] = $value;
+                        $order->info['total'] += $value;
+                        
+                        if ($this->tax_class_id != 0) {
+                            $tax_rate = zen_get_tax_rate($this->tax_class_id);
+                            $misc_tax = zen_calculate_tax($value, $tax_rate);
+                            $order->info['total'] += $misc_tax;
+                            $order->info['tax'] += $misc_tax;
+                            
+                            $tax_description = zen_get_tax_description($this->tax_class_id);
+                            if (!isset($order->info['tax_groups'][$tax_description])) {
+                                $order->info['tax_groups'][$tax_description] = $misc_tax;
+                            } else {
+                                $order->info['tax_groups'][$tax_description] += $misc_tax;
+                            }
+                        }
+                    }
+                    $this->output[] = array(
+                        'title' => $order->totals[$i]['title'] . ':',
+                        'text' => $currencies->format($value, true, $order->info['currency'], $order->info['currency_value']),
+                        'value' => $value
+                    );
+                    break;
                 }
-
-                // Apply the cost to the order.
-                $discount = (float)$query->fields['value'];
-                $order->info['total'] += $discount;
-
-                $products_id = $order->products[0]['id'];
-                $products_info = $db->Execute(
-                    "SELECT products_tax_class_id 
-                       FROM " . TABLE_PRODUCTS . " 
-                      WHERE products_id = $products_id 
-                      LIMIT 1"
-                );
-                $products_tax_rate = zen_get_tax_rate($products_info->fields['products_tax_class_id']);
-                $order->info['total'] -= $order->info['tax'];
-                $order->info['tax'] = zen_calculate_tax($order->info['total'], $products_tax_rate);
-                $order->info['total'] += $order->info['tax'];
-                foreach ($order->info['tax_groups'] as $tax_description => $tax_value) {
-                    $order->info['tax_groups'][$tax_description] = $order->info['tax'];
-                }
-
-                // Output the order total information
-                $this->output[] = array(
-                    'title' => $this->title . ':',
-                    'text' => $currencies->format($discount, true, $order->info['currency'], $order->info['currency_value']),
-                    'value' => (string)$discount,
-                );
-                unset($discount);
             }
         }
     }
@@ -113,7 +118,7 @@ class ot_misc_cost
                   WHERE configuration_key = 'MODULE_ORDER_TOTAL_MISC_COST_STATUS'
                   LIMIT 1"
             );
-            $this->enabled = $check_query->RecordCount();
+            $this->enabled = !$check_query->EOF;
         }
         return $this->enabled;
     }
@@ -122,7 +127,8 @@ class ot_misc_cost
     {
         return array(
             'MODULE_ORDER_TOTAL_MISC_COST_STATUS', 
-            'MODULE_ORDER_TOTAL_MISC_COST_SORT_ORDER', 
+            'MODULE_ORDER_TOTAL_MISC_COST_SORT_ORDER',
+            'MODULE_ORDER_TOTAL_MISC_COST_TAX_CLASS',
             'MODULE_ORDER_TOTAL_MISC_COST_CHANGE_TITLE'
         );
     }
@@ -130,21 +136,38 @@ class ot_misc_cost
     public function install() 
     {
         global $db;
-        $db->Execute("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) VALUES ('This module is installed', 'MODULE_ORDER_TOTAL_MISC_COST_STATUS', 'true', '', '6', '1','zen_cfg_select_option(array(\'true\'), ', now())");
-        $db->Execute("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) VALUES ('Sort Order', 'MODULE_ORDER_TOTAL_MISC_COST_SORT_ORDER', '410', 'Sort order of display.', '6', '2', now())");
-        $db->Execute("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) VALUES ('Allow changing the title', 'MODULE_ORDER_TOTAL_MISC_COST_CHANGE_TITLE', 'false', 'Allow changing the title of the module while editing an order', '6', '3','zen_cfg_select_option(array(\'true\',\'false\'), ', now())");
+        $db->Execute(
+            "INSERT INTO " . TABLE_CONFIGURATION . " 
+                (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) 
+             VALUES 
+                ('This module is installed', 'MODULE_ORDER_TOTAL_MISC_COST_STATUS', 'true', '', '6', '1','zen_cfg_select_option(array(\'true\'), ', now())"
+        );
+        $db->Execute(
+            "INSERT INTO " . TABLE_CONFIGURATION . " 
+                (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) 
+             VALUES 
+                ('Sort Order', 'MODULE_ORDER_TOTAL_MISC_COST_SORT_ORDER', '410', 'Sort order of display.', '6', '2', now())"
+        );
+        $db->Execute(
+            "INSERT INTO " . TABLE_CONFIGURATION . " 
+                (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) 
+             VALUES 
+                ('Allow changing the title', 'MODULE_ORDER_TOTAL_MISC_COST_CHANGE_TITLE', 'false', 'Allow changing the title of the module while editing an order', '6', '3','zen_cfg_select_option(array(\'true\',\'false\'), ', now())"
+         );
+         $db->Execute(
+            "INSERT INTO " . TABLE_CONFIGURATION . " 
+                (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) 
+             VALUES 
+                ('Tax Class', 'MODULE_ORDER_TOTAL_MISC_COST_TAX_CLASS', '0', 'Use the following tax class for any Miscellaneous Cost. If you are applying a tax to this order-total, remember to:<ol><li>Set its <em>Sort Order</em> to a value greater than that for <code>ot_coupon</code>.</li><li>Set its <em>Sort Order</em> to a value less than that for <code>ot_tax</code>.</li></ol>', '6', '7', 'zen_get_tax_class_title', 'zen_cfg_pull_down_tax_classes(', now())"
+          );
      }
 
     public function remove() 
     {
         global $db;
-        $keys = '';
-        $keys_array = $this->keys();
-        for ($i=0; $i<sizeof($keys_array); $i++) {
-          $keys .= "'" . $keys_array[$i] . "',";
-        }
-        $keys = substr($keys, 0, -1);
-
-        $db->Execute("DELETE FROM " . TABLE_CONFIGURATION . " WHERE configuration_key in (" . $keys . ")");
+        $keys = "'" . implode("', '", $this->keys()) . "'";
+        $db->Execute(
+            "DELETE FROM " . TABLE_CONFIGURATION . " 
+              WHERE configuration_key IN (" . $keys . ")");
     }
 }
