84f3d00013f6ca57b32127379b25f1b8f14b3d41
lat9 <cindy@vinosdefrutastropicales.com>
2020-09-23 06:53:46 -0400

    #170: Update init/readme

--- YOUR_ADMIN/includes/init_includes/init_eo_config.php
+++ YOUR_ADMIN/includes/init_includes/init_eo_config.php
@@ -7,7 +7,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('EO_CURRENT_VERSION', '4.5.3-beta1');
+define('EO_CURRENT_VERSION', '4.5.3-beta2');
 
 // -----
 // Only update configuration when an admin is logged in.
