935acddb43fa15fb4ccb2d794543499f5e67f5b5
lat9 <cindy@vinosdefrutastropicales.com>
2020-10-31 08:35:12 -0400

    Update readme/init for v4.5.4 release.

--- YOUR_ADMIN/includes/init_includes/init_eo_config.php
+++ YOUR_ADMIN/includes/init_includes/init_eo_config.php
@@ -7,7 +7,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('EO_CURRENT_VERSION', '4.5.4-beta2');
+define('EO_CURRENT_VERSION', '4.5.4');
 
 // -----
 // Only update configuration when an admin is logged in.
