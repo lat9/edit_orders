eea05d3f8d9fe8b938b5a4672f569557baa20073
lat9 <cindy@vinosdefrutastropicales.com>
2022-03-01 14:54:47 -0500

    #209: Ensure that 'EO_DEBUG_ACTION_LEVEL' setting is recorded into the database.

--- YOUR_ADMIN/includes/init_includes/init_eo_config.php
+++ YOUR_ADMIN/includes/init_includes/init_eo_config.php
@@ -7,7 +7,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('EO_CURRENT_VERSION', '4.6.1-beta3');
+define('EO_CURRENT_VERSION', '4.6.1-beta4');
 
 // -----
 // Only update configuration when an admin is logged in.
@@ -56,21 +56,21 @@ if (!defined('EO_VERSION')) {
          VALUES 
             ( 'Reset Totals on Update &mdash; Default', 'EO_TOTAL_RESET_DEFAULT', 'off', 'Choose the default value for the <em>Reset totals prior to update</em> checkbox.  If your store uses order-total modules that perform tax-related recalculations (like &quot;Group Pricing&quot;), set this value to <b>on</b>.', $cgi, 5, now(), NULL, 'zen_cfg_select_option(array(\'on\', \'off\'),')"
     );
-    
+
     $db->Execute(
         "INSERT INTO " . TABLE_CONFIGURATION . " 
             ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
          VALUES 
             ( 'Use a mock shopping cart?', 'EO_MOCK_SHOPPING_CART', 'true', 'When enabled, a mock shopping cart is created which reads product information from the current order. Many of the 3rd party \"Discount\" Order Total modules were not designed to be run from the Zen Cart administrative interface and require this option to be enabled.<br /><br />The mock shopping cart only provides the get_products and in_cart methods.<br /><br />If installed order total or shipping modules require additional methods from the shopping cart, the mock cart should be disabled.', $cgi, 10, now(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),')"
     );
-  
+
     $db->Execute(
         "INSERT INTO " . TABLE_CONFIGURATION . " 
             ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
          VALUES 
             ( 'Strip tags from the shipping module name?', 'EO_SHIPPING_DROPDOWN_STRIP_TAGS', 'true', 'When enabled, HTML and PHP tags present in the title of a shipping module are removed from the text displayed in the shipping dropdown menu.<br /><br />If partial or broken tags are present in the title it may result in the removal of more text than expected. If this happens, you will need to update the affected shipping module(s) or disable this option.', $cgi, 11, now(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),')"
     );
-  
+
     $db->Execute(
         "INSERT INTO " . TABLE_CONFIGURATION . " 
             ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
@@ -84,16 +84,16 @@ if (!defined('EO_VERSION')) {
          VALUES 
             ( 'Edit Orders File Missing', 'EO_INIT_FILE_MISSING', '0', 'This (hidden) value is set to 1 if <em>EO</em> has detected missing files.', 6, 92, now(), NULL, NULL)"
     );
-    
+
     // -----
     // Register the admin pages for the plugin.
     //
     zen_register_admin_page('editOrders', 'BOX_CONFIGURATION_EDIT_ORDERS', 'FILENAME_EDIT_ORDERS', '', 'customers', 'N');
     zen_register_admin_page('configEditOrders', 'BOX_CONFIGURATION_EDIT_ORDERS', 'FILENAME_CONFIGURATION', "gID=$cgi", 'configuration', 'Y', $cgi);
-    
+
     define('EO_INIT_FILE_MISSING', '1');  //-Set so that the notifier checks will run upon initial installation.
     define('EO_VERSION', '0.0.0');
-    
+
     $messageStack->add(sprintf(EO_INIT_INSTALLED, EO_CURRENT_VERSION), 'success');
 }
 
@@ -152,13 +152,13 @@ if (EO_VERSION != EO_CURRENT_VERSION) {
                        ADD shipping_tax_rate decimal(15,4) default NULL"
                 );
             }
-            
+
             $db->Execute(
                 "DELETE FROM " . TABLE_CONFIGURATION . "
                   WHERE configuration_key = 'EO_MOCK_SHOPPING_CART'
                   LIMIT 1"
             );
-            
+
             $db->Execute(
                 "UPDATE " . TABLE_CONFIGURATION . "
                     SET configuration_description = 'Choose the <em>method</em> that &quot;EO&quot; uses to calculate product prices when an order is updated, one of:<ol><li><b>Auto</b>: Each product-price is re-calculated &mdash; <em>without</em> using any &quot;specials&quot; pricing.  If your products have attributes, this enables changes to a product\'s attributes to automatically update the associated product-price.</li><li><b>AutoSpecials</b>: Each product-price is re-calculated, as above, but using any &quot;specials&quot; pricing.</li><li><b>Manual</b>: Each product-price is based on the <b><i>admin-entered price</i></b> for the product.</li><li><b>Choose</b>: The product-price calculation method varies on an order-by-order basis, via the &quot;tick&quot; of a checkbox.  The default method used (<em>Auto</em> vs. <em>Manual</em> is defined by the <em>Product Price Calculation &mdash; Default</em> setting.</li></ol>',
@@ -172,7 +172,7 @@ if (EO_VERSION != EO_CURRENT_VERSION) {
                   WHERE configuration_key = 'EO_PRODUCT_PRICE_CALC_DEFAULT'
                   LIMIT 1"
             );
-            
+
             $db->Execute(
                 "INSERT IGNORE INTO " . TABLE_CONFIGURATION . " 
                     ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
@@ -184,12 +184,23 @@ if (EO_VERSION != EO_CURRENT_VERSION) {
             $check_init_file_missing = '1';
                                                 //-Fall-through for additional checks
         case (version_compare(EO_VERSION, '4.5.0', '<')):
-            $default_value = (EO_VERSION == '0.0.0') ? 'CSB' : 'CBS';
+            $default_value = (EO_VERSION === '0.0.0') ? 'CSB' : 'CBS';
             $db->Execute(
                 "INSERT IGNORE INTO " . TABLE_CONFIGURATION . " 
                     (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function) 
                  VALUES 
                     ('Addresses, Display Order', 'EO_ADDRESSES_DISPLAY_ORDER', '$default_value', 'In what order, left-to-right, should <em>Edit Orders</em> display an order\'s addresses?  Choose <b>CSB</b> to display <em>Customer</em>, <em>Shipping</em> and then <em>Billing</em>; choose <b>CBS</b> to display <em>Customer</em>, <em>Billing</em> and then <em>Shipping</em>.', $cgi, 1, now(), NULL, 'zen_cfg_select_option(array(\'CSB\', \'CBS\'),')"
+            );
+                                                //-Fall-through for additional checks
+        // -----
+        // Way-old versions of EO didn't have the EO_DEBUG_ACTION_LEVEL setting set.
+        //
+        case (EO_VERSION !== '0.0.0' && !defined('EO_DEBUG_ACTION_LEVEL')):
+            $db->Execute(
+                "INSERT INTO " . TABLE_CONFIGURATION . " 
+                    ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function ) 
+                 VALUES 
+                    ( 'Debug Action Level', 'EO_DEBUG_ACTION_LEVEL', '0', 'When enabled when actions are performed by Edit Orders additional debugging information will be stored in a log file.<br /><br />Enabling debugging will result in a large number of created log files and may adversely affect server performance. Only enable this if absolutely necessary!', $cgi, 12, now(), NULL, 'eo_debug_action_level_list(')"
             );
                                                 //-Fall-through for additional checks
 
@@ -201,7 +212,7 @@ if (EO_VERSION != EO_CURRENT_VERSION) {
     // Update the configuration reflect the current EO version, issuing an updated-message if this wasn't an
     // initial install.
     //
-    if (EO_VERSION != '0.0.0') {
+    if (EO_VERSION !== '0.0.0') {
         $messageStack->add(sprintf(EO_INIT_VERSION_UPDATED, EO_VERSION, EO_CURRENT_VERSION), 'success');
     }
     $db->Execute(
@@ -216,7 +227,7 @@ if (EO_VERSION != EO_CURRENT_VERSION) {
 // On initial installation, upgrade from a version prior to 4.3.0 or if the required notifiers'
 // check previously failed, check for the notifiers required by EO.
 //
-if (EO_INIT_FILE_MISSING == '1' || !empty($check_init_file_missing)) {
+if (EO_INIT_FILE_MISSING === '1' || !empty($check_init_file_missing)) {
     $notifier_check = new \Vinos\Common\NotifierCheck(EO_INIT_MISSING_NOTIFIERS, EO_INIT_MISSING_FILES);
     $notifier_check->setList(
         [
