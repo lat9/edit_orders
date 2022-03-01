268047af778df63269f978961e7267fdbb8311cc
lat9 <cindy@vinosdefrutastropicales.com>
2022-03-01 14:34:12 -0500

    #203: Remove duplicates and 'Ty Package Tracker' language constants.

--- YOUR_ADMIN/includes/languages/english/edit_orders.php
+++ YOUR_ADMIN/includes/languages/english/edit_orders.php
@@ -2,7 +2,7 @@
 // -----
 // Language constants used by the /admin/edit_orders.php processing (Edit Orders).
 //
-//-Last modified 20210317-proseLA Edit Orders v4.6.0
+//-Last modified 20220301-lat9 Edit Orders v4.6.1
 //
 // Page / Section Headings
 define('HEADING_TITLE', 'Editing Order');
@@ -19,7 +19,9 @@ define('TABLE_HEADING_DATE_PURCHASED', 'Date Purchased');
 define('TABLE_HEADING_STATUS', 'Status');
 define('TABLE_HEADING_ACTION', 'Action');
 define('TABLE_HEADING_QUANTITY', 'Qty.');
-define('TABLE_HEADING_PRODUCTS_MODEL', 'Model');
+if ((PROJECT_VERSION_MAJOR . '.' . PROJECT_VERSION_MINOR) < '1.5.7') {
+    define('TABLE_HEADING_PRODUCTS_MODEL', 'Model');
+}
 define('TABLE_HEADING_PRODUCTS', 'Products');
 define('TABLE_HEADING_TAX', 'Tax');
 define('TABLE_HEADING_TOTAL', 'Total');
@@ -67,7 +69,6 @@ define('EMAIL_TEXT_ORDER_NUMBER', 'Order Number:');
 define('EMAIL_TEXT_INVOICE_URL', 'Detailed Invoice:');
 define('EMAIL_TEXT_DATE_ORDERED', 'Date Ordered:');
 define('EMAIL_TEXT_COMMENTS_UPDATE', '<em>The comments for your order are: </em>');
-define('EMAIL_TEXT_COMMENTS_TRACKING_UPDATE', '<em>Items from your order will be shipping soon!</em>');
 define('EMAIL_TEXT_STATUS_UPDATED', 'Your order has been updated to the following status:' . "\n");
 define('EMAIL_TEXT_STATUS_LABEL', '%s' . "\n\n");
 define('EMAIL_TEXT_STATUS_PLEASE_REPLY', 'Please reply to this email if you have any questions.' . "\n");
@@ -75,7 +76,7 @@ define('EMAIL_TEXT_STATUS_PLEASE_REPLY', 'Please reply to this email if you have
 // Success, Warning, and Error Messages
 define('ERROR_ORDER_DOES_NOT_EXIST', 'Error: Order does not exist.');
 define('SUCCESS_ORDER_UPDATED', 'Success: Order has been successfully updated.');
-define('WARNING_DISPLAY_PRICE_WITH_TAX', 'Warning: You have configured Zen Cart to display prices with' . (DISPLAY_PRICE_WITH_TAX_ADMIN != 'true' ? 'out ' : ' ') . 'tax. This page is currently displaying prices with' . (DISPLAY_PRICE_WITH_TAX != 'true' ? 'out ' : ' ') . 'tax.');
+define('WARNING_DISPLAY_PRICE_WITH_TAX', 'Warning: You have configured Zen Cart to display prices with' . (DISPLAY_PRICE_WITH_TAX_ADMIN !== 'true' ? 'out ' : ' ') . 'tax. This page is currently displaying prices with' . (DISPLAY_PRICE_WITH_TAX !== 'true' ? 'out ' : ' ') . 'tax.');
 define('WARNING_ADDRESS_COUNTRY_NOT_FOUND', 'Warning: One or more of the customer address fields contains a country name unknown to Zen Cart (&quot;Locations / Taxes&quot;-&gt;&quot;Countries&quot;).<br />Taxes and some shipping modules may not function correctly until the issue has been resolved.<br /><br />This typically occurs if someone deletes or renames a country&#39;s name from Zen Cart (&quot;Locations / Taxes&quot;-&gt;&quot;Countries&quot;). You can fix the issue by doing one of the following: <ul><li>Add the country (and name) back to the Zen Cart database.</li><li>Adjust the country name to match one of the country names in the Zen Cart database.</li></ul>');
 define('WARNING_ORDER_NOT_UPDATED', 'Warning: Nothing to change. The order was not updated.');
 define('WARNING_ORDER_QTY_OVER_MAX', 'Warning: The quantity requested exceeded the maximum allowed for an order. The quantity added was reduced to the maximum allowed per order.');
@@ -84,7 +85,7 @@ define('WARNING_INSUFFICIENT_PRODUCT_STOCK', 'Insufficient stock for <em>%1$s</e
 
 define('ERROR_ZEN_ADD_TAX_ROUNDING', "The store's <code>zen_add_tax</code> function must be updated to enable <em>Edit Orders</em>' use.");
 
-define ('ERROR_ZC155_NO_SANITIZER', 'You must install the Zen Cart 1.5.5 <em>AdminRequestSanitizer</em> class before you can use Edit Orders on this site.');
+define('ERROR_ZC155_NO_SANITIZER', 'You must install the Zen Cart 1.5.5 <em>AdminRequestSanitizer</em> class before you can use Edit Orders on this site.');
 // Product & Attribute Display
 define('TEXT_ATTRIBUTES_ONE_TIME_CHARGE', 'One Time Charges: &nbsp;&nbsp;');
 define('TEXT_ATTRIBUTES_UPLOAD_NONE', 'No file was uploaded');
@@ -122,13 +123,6 @@ define('DETAILS', 'Details');
 // Required for various added zen_cart functions
 define('TEXT_UNKNOWN_TAX_RATE', 'Tax');
 define('PULL_DOWN_DEFAULT', 'Please Choose Your Country');
-
-// Ty Package Tracker
-define('TABLE_HEADING_TRACKING_ID', 'Tracking ID');
-define('TABLE_HEADING_CARRIER_NAME', 'Carrier');
-define('ENTRY_ADD_TRACK', 'Add Tracking ID');
-define('IMAGE_TRACK', 'Add Tracking ID');
-define('HEADING_TITLE_ORDER_DETAILS', 'Order # ');
 
 // Absolute's Product Attribute Grid
 define('WARNING_ATTRIBUTE_OPTION_GRID', 'Warning: Absolute\'s Product Attribute Grid was detected, however the Product Option Type for Attribute Grid was not fully installed in the database. Temporarily configuring PRODUCTS_OPTIONS_TYPE_ATTRIBUTE_GRID = 23997.');
