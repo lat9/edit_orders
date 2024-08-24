e3d018077287750e0276dea924ad4dc78034022c
lat9 <lat9@vinosdefrutastropicales.com>
2024-08-24 10:09:40 -0400

    Move existing files into the encapsulated directories

---
+++ zc_plugins/EditOrders/v5.0.0/manifest.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'pluginVersion' => 'v5.0.0',
+    'pluginName' => 'Edit Orders',
+    'pluginDescription' => 'Edit Orders (EO) is a Zen Cart admin module that allows administrative users to edit a customer\'s order. See <em>Edit Order</em>\'s <a href="https://github.com/lat9/edit_orders/wiki" target="_blank" rel="noopener noreferrer">Wiki</a> for additional information.',
+    'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
+    'pluginId' => 0, // ID from Zen Cart forum
+    'zcVersions' => [],
+    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => '', // url
+    'pluginGroups' => [],
+];
